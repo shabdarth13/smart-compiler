3f6cc2446cab4da87f302424536d0229bf860b2c
SHABDARTH <shabdarth53@gmail.com>
2026-04-02 11:39:18 +0530

    commit

--- backend/executor.py
+++ backend/executor.py
@@ -1,12 +1,13 @@
 from ast_nodes import *
 
+MAX_ITERATIONS = 1000  
 def eval_expr(node, env):
     if isinstance(node, Number):
         return node.value
 
     if isinstance(node, Variable):
         if node.name not in env:
-            raise Exception(f"Undefined variable {node.name}")
+            raise Exception(f"Semantic Error: Undefined variable '{node.name}'")
         return env[node.name]
 
     if isinstance(node, BinOp):
@@ -15,39 +16,58 @@ def eval_expr(node, env):
 
         if node.op == '+':
             return left + right
+
         elif node.op == '*':
             return left * right
+
         elif node.op == '/':
+            if right == 0:
+                raise Exception("Runtime Error: Division by zero")
             return left // right
+
         elif node.op == '<':
             return left < right
+
         elif node.op == '>':
             return left > right
 
+    raise Exception("Invalid expression")
 def execute(ast, env=None):
     if env is None:
         env = {}
 
     output = []
 
-    for node in ast:
+    def run_block(nodes):
+        nonlocal output
 
-        if isinstance(node, Assign):
-            env[node.var] = eval_expr(node.value, env)
+        for node in nodes:
+            # ASSIGNMENT
+            if isinstance(node, Assign):
+                env[node.var] = eval_expr(node.value, env)
+            # PRINT
+            elif isinstance(node, Print):
+                val = eval_expr(node.expr, env)
+                output.append(str(val))
+            # IF-ELSE
+            elif isinstance(node, IfElse):
+                condition = eval_expr(node.condition, env)
 
-        elif isinstance(node, Print):
-            output.append(str(eval_expr(node.expr, env)))
+                if condition:
+                    run_block(node.if_body)
+                else:
+                    run_block(node.else_body)
+            # WHILE LOOP (SAFE VERSION)
+            elif isinstance(node, WhileLoop):
+                loop_counter = 0
 
-        elif isinstance(node, IfElse):
-            if eval_expr(node.condition, env):
-                out = execute(node.if_body, env)
-            else:
-                out = execute(node.else_body, env)
-            output.extend(out)
+                while eval_expr(node.condition, env):
+                    if loop_counter >= MAX_ITERATIONS:
+                        raise Exception("⚠ Infinite loop detected - execution stopped")
 
-        elif isinstance(node, WhileLoop):
-            while eval_expr(node.condition, env):
-                out = execute(node.body, env)
-                output.extend(out)
+                    run_block(node.body)
 
+                    loop_counter += 1
+
+    run_block(ast)
     return output
